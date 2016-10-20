756e7df8710d7fe0b80057a3dba214e104e3486f
kobimagn2123 <kmagnezi@gmail.com>
2016-10-20 16:50:29 +0300

    kobi try

--- SmartSuper/Controllers/FoodCompanyController.cs
+++ SmartSuper/Controllers/FoodCompanyController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace SmartSuper.Controllers
 {
-    // Adi gay lalal
+    // Adi gay lala
     // Here they will see all the food companies, and if they press one of them, they will see all the products they sell
     public class FoodCompanyController : Controller
     {
