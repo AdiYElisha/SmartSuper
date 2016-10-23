4ea5837b636d82006b7fb0b17354921b8ebe3e8f
AdiYElisha <adiyelisha@gmail.com>
2016-10-24 02:54:25 +0300

    models update and db update

--- SmartSuper/DAL/SmartSuperContext5.cs
+++ SmartSuper/DAL/SmartSuperContext5.cs
@@ -10,21 +10,17 @@ namespace SmartSuper.DAL
 {
     public class SmartSuperContext5 : DbContext
     {
-        public SmartSuperContext5() : base("SmartSuperContext5")
+        public SmartSuperContext5() : base("SmartSuperContext11")
         { }
 
         public DbSet<FoodCompanies> FoodCompanies { get; set; }
         public DbSet<Customers> Customer { get; set; }
         public DbSet<Products> Products { get; set; }
-        //public DbSet<ProductType> ProductTypes { get; set; }
-        //public DbSet<ShoppingCart> ShoppingCarts { get; set; }
         public DbSet<Supers> Supers { get; set; }
+        public DbSet<ShoppingCart_Product> ShoppingCart_Product { get; set; }
+        public DbSet<Product_Super> Product_Super { get; set; }
 
         public System.Data.Entity.DbSet<SmartSuper.Models.ProductKinds> ProductKinds { get; set; }
-
-        //public System.Data.Entity.DbSet<SmartSuper.Models.Product> Products { get; set; }
-
-
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder) // Tables name in RABIM 
         { 
--- SmartSuper/Models/Product_Super.cs
+++ SmartSuper/Models/Product_Super.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace SmartSuper.Models
 {
     public class Product_Super
     {
+        [Key]
+        [Column(Order = 1)]
         public int Product_ID { get; set; }
+        [Key]
+        [Column(Order = 2)]
         public int Super_ID { get; set; }
+        [Display(Name = "מחיר המוצר")]
+        [Required(ErrorMessage = "שדה חובה")]
         public int Price { get; set; }
     }
 }
